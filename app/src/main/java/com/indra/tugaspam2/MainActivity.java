1e533d6dce08c99579bd3016cdbac1072f964a01
seregant <indraagus@live.com>
2018-10-10 09:41:14 +0700

    add camera input

--- app/src/main/java/com/indra/tugaspam2/MainActivity.java
+++ app/src/main/java/com/indra/tugaspam2/MainActivity.java
@@ -1,13 +1,20 @@
 package com.indra.tugaspam2;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    Bitmap photo;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -16,17 +23,46 @@ public class MainActivity extends AppCompatActivity {
         final EditText msgNim = (EditText) findViewById(R.id.nim);
         final EditText msgAlamat = (EditText) findViewById(R.id.alamat);
         final EditText msgNoTelp = (EditText) findViewById(R.id.telp);
+        final EditText msgMail = (EditText) findViewById(R.id.email);
+
         Button btnSubmit = (Button) findViewById(R.id.submit);
+        Button btnCamera = (Button) findViewById(R.id.takePhoto);
+        final Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        final Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+                onActivityResult(1, 1, takePictureIntent);
+            }
+        });
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                if (photo!= null){
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    photo.compress(Bitmap.CompressFormat.PNG, 90, stream);
+                    byte[] msgImage = stream.toByteArray();
+                    intent.putExtra("img", msgImage);
+                }
                 intent.putExtra("nama", msgNama.getText().toString());
                 intent.putExtra("nim", msgNim.getText().toString());
                 intent.putExtra("alamat", msgAlamat.getText().toString());
                 intent.putExtra("telp", msgNoTelp.getText().toString());
+                intent.putExtra("mail", msgMail.getText().toString());
                 startActivity(intent);
             }
         });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final ImageView thmb = (ImageView) findViewById(R.id.thumb);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            thmb.setImageBitmap(imageBitmap);
+            this.photo = (Bitmap) extras.get("data");
+        }
     }
 }
--- app/src/main/java/com/indra/tugaspam2/SecondActivity.java
+++ app/src/main/java/com/indra/tugaspam2/SecondActivity.java
@@ -1,29 +1,37 @@
 package com.indra.tugaspam2;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_second_activity);
         TextView vNama = (TextView)findViewById(R.id.vnama);
         TextView vNim = (TextView)findViewById(R.id.vnim);
         TextView vAlamat = (TextView)findViewById(R.id.valamat);
         TextView vTelp = (TextView)findViewById(R.id.vtelp);
-
+        TextView vMail = (TextView) findViewById(R.id.vmail);
+        ImageView vImg = (ImageView) findViewById(R.id.vthumb);
 
         Intent data = getIntent();
+        byte[] bitmapData = data.getByteArrayExtra("img");
+        Bitmap imgShow = (Bitmap) BitmapFactory.decodeByteArray(bitmapData, 0, bitmapData.length);
         vNama.setText(data.getStringExtra("nama"));
         vNim.setText(data.getStringExtra("nim"));
         vAlamat.setText(data.getStringExtra("alamat"));
         vTelp.setText(data.getStringExtra("telp"));
-
+        vMail.setText(data.getStringExtra("mail"));
+        vImg.setImageBitmap(imgShow);
     }
 
 }
